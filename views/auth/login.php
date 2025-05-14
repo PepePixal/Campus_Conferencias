64fcdd10760e183104e90f84a77305420a6598dc
PepePixal <pepepixal@gmail.com>
2025-05-14 18:32:34 +0200

    hasta vid.672

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,35 @@
-<h1><?php echo $titulo?></h1>
+<main class="auth">
+    <h2 class="auth__heading"><?php echo $titulo; ?></h2>
+    <p class="auth__texto">Inciar sesión en Campus</p>
+
+    <form class="formulario">
+        <div class="formulario__campo">
+            <label class="formulario__label" for="email">Email</label>
+            <input 
+                class="formulario__input"
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Tu Email"
+            />    
+        </div>
+    
+        <div class="formulario__campo">
+            <label class="formulario__label" for="email">Password</label>
+            <input 
+            class="formulario__input"
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Tu Password"
+            />    
+        </div>
+        
+        <input type="submit" class="formulario__submit" value="Iniciar Sesión">
+    </form>
+
+    <div class="acciones">
+        <a href="/registro" class="acciones__enlace">¿No tienes cuenta? Creala</a>
+        <a href="/olvide" class="acciones__enlace">¿Olvidé mi Password?</a>
+    </div>
+</main>
