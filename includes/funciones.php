204b09fac02d1a3d84a6230bb8db4b4d9bb5e428
PepePixal <pepepixal@gmail.com>
2025-05-19 13:10:22 +0200

    dashboard sidebar contenido hasta vid.694

--- includes/funciones.php
+++ includes/funciones.php
@@ -10,3 +10,10 @@ function s($html) : string {
     $s = htmlspecialchars($html);
     return $s;
 }
+
+//función que recibe una ruta $path y comprueba si el valor string de la ruta actual, 
+//obtenida de $_SERVER['PATH_INF], contiene el valor string de $path, recibido como argumento,
+//retorna un : bool: Si es que si ? retorna true (1), de lo contrario : retorna false (nada);
+function pagina_actual ($path) : bool{
+    return str_contains( $_SERVER['PATH_INFO'], $path) ? true : false;
+}; 
--- views/templates/admin-sidebar.php
+++ views/templates/admin-sidebar.php
@@ -1,22 +1,25 @@
 <aside class="dashboard__sidebar">
     <nav class=dashboard__menu>
-        <a href="/admin/dashboard" class="dashboard__enlace">
+        <!-- el segundo valor de class, es el resultado booleano (1 o nada), retornado por la función pagina_actual(),
+        si es true ? agrega a la clase el valor 'dashboard__enlace--actual, de lo contrario : no gregar nada ''.
+        el valor agregado usaremos para dejar resaltado el enlace que corresponda a la página actual, con css -->
+        <a href="/admin/dashboard" class="dashboard__enlace <?php echo pagina_actual('/dashboard') ? 'dashboard__enlace--actual' : ''; ?>">
             <i class="fa-solid fa-house dashboard__icono"></i>
             <span class="dashboard__menu-texto">Inicio</span>
         </a>
-        <a href="/admin/ponentes" class="dashboard__enlace">
+        <a href="/admin/ponentes" class="dashboard__enlace <?php echo pagina_actual('/ponentes') ? 'dashboard__enlace--actual' : ''; ?>">
             <i class="fa-solid fa-microphone dashboard__icono"></i>
             <span class="dashboard__menu-texto">Ponentes</span>
         </a>    
-        <a href="/admin/eventos" class="dashboard__enlace">
+        <a href="/admin/eventos" class="dashboard__enlace <?php echo pagina_actual('/eventos') ? 'dashboard__enlace--actual' : ''; ?>">
             <i class="fa-solid fa-calendar dashboard__icono"></i>
             <span class="dashboard__menu-texto">Eventos</span>
         </a>    
-        <a href="/admin/registrados" class="dashboard__enlace">
+        <a href="/admin/registrados" class="dashboard__enlace <?php echo pagina_actual('/registrados') ? 'dashboard__enlace--actual' : ''; ?>">
             <i class="fa-solid fa-users dashboard__icono"></i>
             <span class="dashboard__menu-texto">Registrados</span>
         </a>    
-        <a href="/admin/regalos" class="dashboard__enlace">
+        <a href="/admin/regalos" class="dashboard__enlace <?php echo pagina_actual('/regalos') ? 'dashboard__enlace--actual' : ''; ?>">
             <i class="fa-solid fa-gift dashboard__icono"></i>
             <span class="dashboard__menu-texto">Regalos</span>
         </a>    
