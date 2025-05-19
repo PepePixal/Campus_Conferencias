517e3c6b6ac92a17ff863790d6ee6fb1f1eec0e3
PepePixal <pepepixal@gmail.com>
2025-05-19 11:34:46 +0200

    Routing y Controllers admin hasta vid.693

--- Router.php
+++ Router.php
@@ -36,18 +36,41 @@ class Router
         }
     }
 
+    //recibe la vista y los datos
     public function render($view, $datos = [])
     {
         foreach ($datos as $key => $value) {
             $$key = $value; 
         }
 
+        //inicia el almacenamiento en búfer de salida para
+        //capturar la ejecución del archivo de la vista
         ob_start(); 
 
+        //carga la vista .php de /views/, según el argumento $view, 
         include_once __DIR__ . "/views/$view.php";
 
-        $contenido = ob_get_clean(); // Limpia el Buffer
+        //captura el contenido de la vista y lo almacena en $contenido
+        //y despues limpia el búfer.
+        $contenido = ob_get_clean();
 
-        include_once __DIR__ . '/views/layout.php';
+
+        //** Utilizar un Layout (vista principal) según la URL */
+        //obtiene la URL actual, tipo string
+        $url_actual = $_SERVER['PATH_INFO'] ?? '/';
+
+        //si el string en $url_actual, contiene '/admin'
+        if(str_contains($url_actual, '/admin')) {
+            //carga el layout base administración, de la página para usuario admin,
+            // donde se integrará la vista recibida que está almacenada en $contenido.
+            include_once __DIR__ . '/views/admin-layout.php';
+        
+        //si el string en $url_actual, NO contiene '/admin'
+        } else {
+            //carga el layout base normal, de la página para usuario no admin,
+            // donde se integrará la vista recibida que está almacenada en $contenido.
+            include_once __DIR__ . '/views/layout.php';
+        }
+
     }
 }
---
+++ views/templates/admin-header.php
@@ -0,0 +1,13 @@
+<header class="dashboard__header">
+    <div class="dashboard__header-grid">
+        <a href="/">
+            <h3 class="dashboard__logo"> &#60;CampusDevWeb /></h3>
+        </a>
+
+        <nav class="dashboard__nav">
+            <form method="POST" action="/logout" class="dashboard__form">
+                <input type="submit" value="Cerrar Sesión" class="dashboard__submit--logout">
+            </form>
+        </nav>
+    </div>
+</header>
